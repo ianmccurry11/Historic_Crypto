d7c3d312c2d9acee5e50f745560ae90e376725b0
akhil <aelamanc@calpoly.edu>
2024-05-28 16:58:06 -0700

    caesar ciphers done

--- caesar.py
+++ caesar.py
@@ -18,12 +18,14 @@ def decrypt_from_file(file_path):
     with open(file_path, 'r') as file:
         ciphertext = file.read()
     
-    decrypted_file_path = "caesar_easy_2_decrypted.txt"
+    decrypted_file_path = "caesar_hard_2_decrypted.txt"
     with open(decrypted_file_path, 'w') as decrypted_file:
         for shift in range(26):
             decrypted_text = caesar_decrypt(ciphertext, shift)
             decrypted_file.write("Shift: {}\n".format(shift))
             decrypted_file.write("Decrypted Text:\n{}\n".format(decrypted_text))
 
-file_path = "encrypted4/caesar_easy_2_encrypted.txt"
+file_path = "encrypted4/caesar_hard_2_encrypt.txt"
 decrypt_from_file(file_path)
+
+
